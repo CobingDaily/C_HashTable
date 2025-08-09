523b8d40bb99d652f10fa377b2991527be2ad3c7
CobingDaily <thejamecool@gmail.com>
2025-08-10 00:17:07 +0300

    Function declarations for binary tree

--- include/bitree.h
+++ include/bitree.h
@@ -1,9 +1,15 @@
 #pragma once
 
 struct BiTreeNode {
-    int value;
+    int data;
     struct BiTreeNode* leftChild;
     struct BiTreeNode* rightChild;
 };
 
 typedef struct BiTreeNode bi_tree;
+
+bi_tree* new_bi_tree(int data);
+
+bi_tree* bi_tree_insert(bi_tree* root, int data);
+
+void free_bi_tree(bi_tree* root);
